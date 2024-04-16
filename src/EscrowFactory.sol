e714a274ef138bd3f715b6da539b594cef627583
VitalR <vitalmountlabs@gmail.com>
2024-04-16 23:11:48 +0300

    docs: add natspec comments for factory & registry

--- src/EscrowFactory.sol
+++ src/EscrowFactory.sol
@@ -8,13 +8,20 @@ import {IEscrowFactory} from "./interfaces/IEscrowFactory.sol";
 import {IRegistry} from "./interfaces/IRegistry.sol";
 import {Escrow} from "./Escrow.sol";
 
+/// @title Escrow Factory Contract
+/// @dev This contract is used for creating new escrow contract instances using the clone factory pattern.
 contract EscrowFactory is IEscrowFactory, Owned, Pausable {
+    /// @notice Registry contract address storing escrow templates and configurations.
     IRegistry public registry;
 
+    /// @notice Tracks the number of escrows deployed per deployer to generate unique salts for clones.
     mapping(address deployer => uint256 nonce) public factoryNonce;
 
+    /// @notice Tracks the addresses of deployed escrow contracts.
     mapping(address escrow => bool deployed) public existingEscrow;
 
+    /// @dev Sets the initial registry used for cloning escrow contracts.
+    /// @param _registry Address of the registry contract.
     constructor(address _registry) Owned(msg.sender) {
         if (_registry == address(0)) {
             revert Factory__ZeroAddressProvided();
@@ -22,6 +29,15 @@ contract EscrowFactory is IEscrowFactory, Owned, Pausable {
         registry = IRegistry(_registry);
     }
 
+    /// @notice Deploys a new escrow contract clone with unique settings for each project.
+    /// @param _client The client's address who initiates the escrow.
+    /// @param _treasury The treasury's address where fees are collected.
+    /// @param _admin The admin's address who has administrative privileges over the escrow.
+    /// @param _registry Address of the registry contract to fetch escrow implementation.
+    /// @param _feeClient Fee percentage to be paid by the client.
+    /// @param _feeContractor Fee percentage to be paid by the contractor.
+    /// @return deployedProxy The address of the newly deployed escrow proxy.
+    /// @dev Clones the escrow template and initializes it with specific parameters for the project.
     function deployEscrow(
         address _client, //msg.sender
         address _treasury,
@@ -47,16 +63,20 @@ contract EscrowFactory is IEscrowFactory, Owned, Pausable {
         emit EscrowProxyDeployed(msg.sender, deployedProxy);
     }
 
+    /// @notice Updates the registry address used for fetching escrow implementations.
+    /// @param _registry New registry address.
     function updateRegistry(address _registry) external onlyOwner {
         if (_registry == address(0)) revert Factory__ZeroAddressProvided();
         registry = IRegistry(_registry);
         emit RegistryUpdated(_registry);
     }
 
+    /// @notice Pauses the contract, preventing new escrows from being deployed.
     function pause() public onlyOwner {
         _pause();
     }
 
+    /// @notice Unpauses the contract, allowing new escrows to be deployed.
     function unpause() public onlyOwner {
         _unpause();
     }
--- src/Registry.sol
+++ src/Registry.sol
@@ -4,20 +4,26 @@ pragma solidity ^0.8.24;
 import {IRegistry} from "./interfaces/IRegistry.sol";
 import {Owned} from "./libs/Owned.sol";
 
+/// @title Registry Contract
+/// @dev This contract manages configuration settings for the escrow system including approved payment tokens.
 contract Registry is IRegistry, Owned {
-    /// @dev The address interpreted as native token of the chain.
+    /// @notice Constant for the native token of the chain.
+    /// @dev Used to represent the native blockchain currency in payment tokens mapping.
     address public constant NATIVE_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-    /// @dev The Escrow contract address.
+    /// @notice Address of the Escrow contract currently in use.
+    /// @dev This can be updated by the owner as new versions of the Escrow contract are deployed.
     address public escrow;
 
-    /// @notice Mapping of the enabled ERC20 token addresses as a payment token.
-    /// @dev The NATIVE_TOKEN constant should be enabled for facilitating payments using the native token of the chain.
+    /// @notice Mapping of ERC20 token addresses that are enabled as payment options.
+    /// @dev Includes the ability to enable the native chain token for payments.
     mapping(address token => bool enabled) public paymentTokens;
 
+    /// @dev Initializes the contract setting the owner to the message sender.
     constructor() Owned(msg.sender) {}
 
-    /// @notice Method for adding payment token.
+    /// @notice Adds a new ERC20 token to the list of accepted payment tokens.
+    /// @param _token The address of the ERC20 token to enable.
     function addPaymentToken(address _token) external onlyOwner {
         if (_token == address(0)) revert Registry__ZeroAddressProvided();
         if (paymentTokens[_token]) revert Registry__TokenAlreadyAdded();
@@ -25,14 +31,16 @@ contract Registry is IRegistry, Owned {
         emit PaymentTokenAdded(_token);
     }
 
-    /// @notice Method for removing payment token.
+    /// @notice Removes an ERC20 token from the list of accepted payment tokens.
+    /// @param _token The address of the ERC20 token to disable.
     function removePaymentToken(address _token) external onlyOwner {
         if (!paymentTokens[_token]) revert Registry__PaymentTokenNotRegistered();
         delete paymentTokens[_token];
         emit PaymentTokenRemoved(_token);
     }
 
-    /// @notice Method for registering escrow contract address.
+    /// @notice Updates the address of the Escrow contract.
+    /// @param _escrow The new address of the Escrow contract to be used.
     function updateEscrow(address _escrow) external onlyOwner {
         if (_escrow == address(0)) revert Registry__ZeroAddressProvided();
         escrow = _escrow;
