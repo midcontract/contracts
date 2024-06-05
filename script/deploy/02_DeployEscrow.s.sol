99eb5c266437c4b982b9f9522da0ba1e55e0d3f0
VitalR <vitalmountlabs@gmail.com>
2024-06-05 23:55:09 +0300

    script: update escrow deployment script

--- script/deploy/02_DeployEscrow.s.sol
+++ script/deploy/02_DeployEscrow.s.sol
@@ -10,10 +10,14 @@ import {EthSepoliaConfig} from "config/EthSepoliaConfig.sol";
 contract DeployEscrowScript is Script {
     Escrow public escrow;
     address public registry;
+    address public ownerPublicKey;
+    uint256 public ownerPrivateKey;
     address public deployerPublicKey;
     uint256 public deployerPrivateKey;
 
     function setUp() public {
+        ownerPublicKey = vm.envAddress("OWNER_PUBLIC_KEY");
+        ownerPrivateKey = vm.envUint("OWNER_PRIVATE_KEY");
         deployerPublicKey = vm.envAddress("DEPLOYER_PUBLIC_KEY");
         deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         registry = EthSepoliaConfig.REGISTRY;
@@ -23,6 +27,9 @@ contract DeployEscrowScript is Script {
         vm.startBroadcast(deployerPrivateKey);
         escrow = new Escrow();
         escrow.initialize(address(deployerPublicKey), address(deployerPublicKey), address(registry));
+        vm.stopBroadcast();
+
+        vm.startBroadcast(ownerPrivateKey);
         Registry(registry).updateEscrow(address(escrow));
         console.log("==escrow addr=%s", address(escrow));
         assert(address(escrow) != address(0));
