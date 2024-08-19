84aec03db6c0980672bbb894a0543e56b4a2875a
VitalR <vitalmountlabs@gmail.com>
2024-08-20 01:54:43 +0300

    chore: setup new registry & hourly contracts

--- script/deploy/01_DeployRegistry.s.sol
+++ script/deploy/01_DeployRegistry.s.sol
@@ -10,7 +10,7 @@ import {EthSepoliaConfig} from "config/EthSepoliaConfig.sol";
 import {PolAmoyConfig} from "config/PolAmoyConfig.sol";
 
 contract DeployRegistryScript is Script {
-    // EscrowRegistry registry;
+    EscrowRegistry registry;
     MockDAI daiToken;
     MockUSDT usdtToken;
     address ownerPublicKey;
@@ -18,7 +18,7 @@ contract DeployRegistryScript is Script {
     address deployerPublicKey;
     uint256 deployerPrivateKey;
     address paymentToken;
-    address registry;
+    // address registry;
 
     function setUp() public {
         ownerPublicKey = vm.envAddress("OWNER_PUBLIC_KEY");
@@ -26,13 +26,13 @@ contract DeployRegistryScript is Script {
         deployerPublicKey = vm.envAddress("DEPLOYER_PUBLIC_KEY");
         deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         paymentToken = EthSepoliaConfig.MOCK_USDT;
-        registry = PolAmoyConfig.REGISTRY;
+        // registry = PolAmoyConfig.REGISTRY;
     }
 
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
-        // registry = new EscrowRegistry(ownerPublicKey);
-        daiToken = new MockDAI();
+        registry = new EscrowRegistry(ownerPublicKey);
+        // daiToken = new MockDAI();
         // registry.addPaymentToken(address(daiToken));
         // usdtToken = new MockUSDT();
         // console.log("==registry addr=%s", address(registry));
@@ -43,11 +43,15 @@ contract DeployRegistryScript is Script {
         vm.stopBroadcast();
 
         vm.startBroadcast(ownerPrivateKey);
-        EscrowRegistry(registry).addPaymentToken(address(daiToken));
-        // registry.addPaymentToken(address(usdtToken));
-        // registry.setTreasury(ownerPublicKey);
-        // assert(registry.paymentTokens(address(usdtToken)) == true);
-        // assert(registry.treasury() == address(ownerPublicKey));
+        EscrowRegistry(registry).setTreasury(ownerPublicKey);
+        EscrowRegistry(registry).addPaymentToken(0xa801061f49970Ef796e0fD0998348f3436ccCb1d);
+        EscrowRegistry(registry).updateEscrowFixedPrice(0xD8038Fae596CDC13cC9b3681A6Eb44cC1984D670);
+        EscrowRegistry(registry).updateEscrowMilestone(0x2dc075B51ef3b4f0AD868b0cc342951682019E62);
+        EscrowRegistry(registry).updateEscrowHourly(0x2b8660f1c512dBc74967d35BC23A6186a5CDE90a);
+        EscrowRegistry(registry).updateFactory(0xeaD5265B6412103d316b6389c0c15EBA82a0cbDa);
+        EscrowRegistry(registry).updateFeeManager(0xA4857B1178425cfaaaeedBcFc220F242b4A518fA);
+
+        // EscrowRegistry(registry).updateEscrowHourly(address(escrow));
         vm.stopBroadcast();
     }
 }
