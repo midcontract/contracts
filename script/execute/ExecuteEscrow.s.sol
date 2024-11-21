25c5b6be62a0c5d533924841128f787894b6b38c
VitalR <vitalmountlabs@gmail.com>
2024-11-21 15:58:15 +0200

    script: update escrow execution script

--- script/execute/ExecuteEscrow.s.sol
+++ script/execute/ExecuteEscrow.s.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.25;
 import { Script, console } from "forge-std/Script.sol";
 
 import { EscrowFixedPrice, IEscrowFixedPrice } from "src/EscrowFixedPrice.sol";
+import { EscrowHourly, IEscrowHourly } from "src/EscrowHourly.sol";
 import { EscrowFactory, IEscrowFactory } from "src/EscrowFactory.sol";
+import { EscrowAdminManager, OwnedRoles } from "src/modules/EscrowAdminManager.sol";
 import { EscrowFeeManager } from "src/modules/EscrowFeeManager.sol";
 import { EscrowRegistry, IEscrowRegistry } from "src/modules/EscrowRegistry.sol";
 import { EthSepoliaConfig } from "config/EthSepoliaConfig.sol";
@@ -15,14 +17,17 @@ import { MockUSDT } from "test/mocks/MockUSDT.sol";
 
 contract ExecuteEscrowScript is Script {
     address escrow;
+    address escrowHourly;
     address factory;
     address registry;
+    address adminManager;
     address feeManager;
     address usdtToken;
     address owner;
     address newOwner;
 
     IEscrowFixedPrice.Deposit deposit;
+    IEscrowHourly.Deposit depositHourly;
     Enums.FeeConfig feeConfig;
     Enums.Status status;
     Enums.EscrowType escrowType;
@@ -50,11 +55,13 @@ contract ExecuteEscrowScript is Script {
         owner = vm.envAddress("OWNER_PUBLIC_KEY");
 
         escrow = PolAmoyConfig.ESCROW_FIXED_PRICE;
-        registry = PolAmoyConfig.REGISTRY;
-        factory = PolAmoyConfig.FACTORY;
+        registry = PolAmoyConfig.REGISTRY_1;
+        factory = PolAmoyConfig.FACTORY_1;
         feeManager = PolAmoyConfig.FEE_MANAGER;
         newOwner = PolAmoyConfig.OWNER;
         usdtToken = PolAmoyConfig.MOCK_USDT;
+        escrowHourly = PolAmoyConfig.ESCROW_HOURLY_1;
+        adminManager = PolAmoyConfig.ADMIN_MANAGER;
 
         contractData = bytes("contract_data");
         salt = keccak256(abi.encodePacked(uint256(42)));
@@ -75,6 +82,12 @@ contract ExecuteEscrowScript is Script {
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
 
+        deploy_and_deposit_escrow_hourly();
+
+        vm.stopBroadcast();
+    }
+
+    function deploy_and_deposit_escrow_fixed_price() public {
         // EscrowRegistry(registry).transferOwnership(newOwner);
         // assert(address(EscrowFixedPrice(escrow).owner()) == deployerPublicKey);
         // EscrowFixedPrice(escrow).transferOwnership(newOwner);
@@ -86,7 +99,7 @@ contract ExecuteEscrowScript is Script {
 
         EscrowFeeManager(feeManager).transferOwnership(newOwner);
 
-        (bool sent,) = newOwner.call{value: 0.055 ether}("");
+        (bool sent,) = newOwner.call{ value: 0.055 ether }("");
         require(sent, "Failed to send Ether");
 
         // // set treasury
@@ -119,7 +132,35 @@ contract ExecuteEscrowScript is Script {
 
         // // claim
         EscrowFixedPrice(escrowProxy).claim(currentContractId);
+    }
 
-        vm.stopBroadcast();
+    function deploy_and_deposit_escrow_hourly() public {
+        address deployedEscrowProxy;
+        escrowType = Enums.EscrowType.HOURLY;
+        deployedEscrowProxy =
+            EscrowFactory(factory).deployEscrow(escrowType, deployerPublicKey, address(adminManager), address(registry));
+        EscrowHourly escrowProxyHourly = EscrowHourly(address(deployedEscrowProxy));
+        // mint, approve payment token
+        MockUSDT(usdtToken).mint(address(deployerPublicKey), 1300e6);
+        MockUSDT(usdtToken).approve(address(escrowProxyHourly), 1300e6);
+        // deposit
+        uint256 depositHourlyAmount = 1000e6;
+        depositHourly = IEscrowHourly.Deposit({
+            contractor: deployerPublicKey,
+            paymentToken: address(usdtToken),
+            prepaymentAmount: depositHourlyAmount,
+            amountToClaim: 0,
+            feeConfig: Enums.FeeConfig.CLIENT_COVERS_ONLY
+        });
+        uint256 currentContractId = escrowProxyHourly.getCurrentContractId();
+        (uint256 totalDepositAmount, uint256 feeApplied) = EscrowFeeManager(feeManager).computeDepositAmountAndFee(
+            address(escrowProxyHourly),
+            currentContractId,
+            deployerPublicKey,
+            depositHourlyAmount,
+            Enums.FeeConfig.CLIENT_COVERS_ONLY
+        );
+        assert(totalDepositAmount > depositHourlyAmount);
+        escrowProxyHourly.deposit(currentContractId, depositHourly);
     }
 }
