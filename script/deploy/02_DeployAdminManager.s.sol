af03db973541296c164cb68c7ba62dc772193269
VitalR <vitalmountlabs@gmail.com>
2025-01-23 23:18:36 +0200

    chore: update with post audit version of contracts

--- script/deploy/02_DeployAdminManager.s.sol
+++ script/deploy/02_DeployAdminManager.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.25;
 import { Script, console } from "forge-std/Script.sol";
 
 import { EscrowAdminManager } from "src/modules/EscrowAdminManager.sol";
+import { EscrowRegistry } from "src/modules/EscrowRegistry.sol";
 import { EthSepoliaConfig } from "config/EthSepoliaConfig.sol";
 import { PolAmoyConfig } from "config/PolAmoyConfig.sol";
 
@@ -13,18 +14,25 @@ contract DeployAdminManagerScript is Script {
     uint256 ownerPrivateKey;
     address deployerPublicKey;
     uint256 deployerPrivateKey;
+    address registry;
 
     function setUp() public {
         ownerPublicKey = vm.envAddress("OWNER_PUBLIC_KEY");
         ownerPrivateKey = vm.envUint("OWNER_PRIVATE_KEY");
         deployerPublicKey = vm.envAddress("DEPLOYER_PUBLIC_KEY");
         deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        registry = PolAmoyConfig.REGISTRY;
     }
 
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
         adminManager = new EscrowAdminManager(ownerPublicKey);
         console.log("==adminManager addr=%s", address(adminManager));
+        vm.stopBroadcast();
+
+        vm.startBroadcast(ownerPrivateKey);
+        EscrowRegistry(registry).setAdminManager(address(adminManager));
+        assert(EscrowRegistry(registry).adminManager() == address(adminManager));
         vm.stopBroadcast();
     }
 }
